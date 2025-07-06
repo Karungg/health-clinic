b16d5fdb3538c495ec503f47d461bb030861a299
Karungg <miftahfadilah71@gmail.com>
2025-07-06 18:30:19 +0700

    Add some test in user.java

--- src/test/java/healthclinic/health_clinic/models/UserTest.java
+++ src/test/java/healthclinic/health_clinic/models/UserTest.java
@@ -1,8 +1,19 @@
 package healthclinic.health_clinic.models;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class UserTest {
 
+    @Test
+    void createUser() {
+        User user = new User();
+        user.setUsername("user 1");
+        user.setPassword("password");
+
+        assertNotNull(user);
+    }
 }
