f6be598b25aa3ca65e87c47387cb98f457e8fd74
Karungg <miftahfadilah71@gmail.com>
2025-07-30 05:44:31 +0700

    Refactor controller advice

--- src/main/java/healthclinic/health_clinic/controllers/ErrorControllerAdvice.java
+++ src/main/java/healthclinic/health_clinic/controllers/ErrorControllerAdvice.java
@@ -1,18 +1,19 @@
 package healthclinic.health_clinic.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.lang.IllegalArgumentException;
 
+import healthclinic.health_clinic.dto.GenericResponse;
 import healthclinic.health_clinic.exception.ResourceNotFoundException;
 
 @RestControllerAdvice
@@ -34,16 +35,16 @@ public class ErrorControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        Map<String, String> errors = new HashMap<>();
+    public GenericResponse<Map<String, List<String>>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        Map<String, List<String>> errors = new HashMap<>();
 
-        exception.getBindingResult().getAllErrors().forEach(error -> {
+        exception.getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
+            errors.computeIfAbsent(fieldName, k -> new ArrayList<>()).add(errorMessage);
         });
 
-        return errors;
+        return GenericResponse.badRequest(errors);
     }
-
 }
--- src/main/java/healthclinic/health_clinic/dto/GenericResponse.java
+++ src/main/java/healthclinic/health_clinic/dto/GenericResponse.java
@@ -1,8 +1,15 @@
 package healthclinic.health_clinic.dto;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,24 +17,41 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GenericResponse<T> {
 
     private Integer code;
     private String status;
     private T Data;
+    private Object errors;
+
+    public static <T> GenericResponse<T> success(Integer code, String status, T data) {
+        return GenericResponse.<T>builder()
+                .code(code)
+                .status(status)
+                .Data(data)
+                .build();
+    }
+
+    public static <T> GenericResponse<T> error(Integer code, String status, Object errors) {
+        return GenericResponse.<T>builder()
+                .code(code)
+                .status(status)
+                .errors(errors)
+                .build();
+    }
 
     public static <T> GenericResponse<T> ok(T data) {
-        return new GenericResponse<>(
-                HttpStatus.OK.value(),
-                HttpStatus.OK.getReasonPhrase(),
-                data);
+        return success(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), data);
     }
 
     public static <T> GenericResponse<T> created(T data) {
-        return new GenericResponse<>(
-                HttpStatus.CREATED.value(),
-                HttpStatus.CREATED.getReasonPhrase(),
-                data);
+        return success(HttpStatus.CREATED.value(), HttpStatus.CREATED.getReasonPhrase(), data);
+    }
+
+    public static <T> GenericResponse<T> badRequest(Map<String, List<String>> errors) {
+        return error(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), errors);
     }
 }
