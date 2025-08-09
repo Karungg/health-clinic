5e0e0f00a146c77b57f57afb15386518555a8fbe
Karungg <miftahfadilah71@gmail.com>
2025-08-09 19:34:51 +0700

    Update medicines

--- src/main/java/healthclinic/health_clinic/models/Medicine.java
+++ src/main/java/healthclinic/health_clinic/models/Medicine.java
@@ -1,8 +1,31 @@
 package healthclinic.health_clinic.models;
 
-import jakarta.persistence.Entity;
+import java.util.UUID;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@EntityListeners({ AuditingEntityListener.class })
 @Entity
+@Table(name = "medicines")
+@Data
 public class Medicine {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+
 }
